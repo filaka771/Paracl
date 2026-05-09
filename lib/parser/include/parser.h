c6d1d88cb4240eb1dfbba93db78b23301d47355f
Alex Filak <filaka771@gmail.com>
2026-05-09 09:02:47 +0300

    Statements parsers implemented

--- lib/parser/include/parser.h
+++ lib/parser/include/parser.h
@@ -99,42 +99,156 @@ private:
     std::tuple<TokenIter, TokenIter> parameter_list_;
 };
 
-class CompoundStmt : public Node {
+
+//----------------Statements----------------
+class ExpressionStmt : public Node {
 public:
-    CompoundStmt(
-        Node* parent_ptr,
-        std::tuple<TokenIter, TokenIter> compound_stmt
+    ExpressionStmt(
+        std::tuple<TokenIter, TokenIter> stmt_range,
+        std::tuple<TokenIter, TokenIter> expr_range
     )
-    : Node("CompoundStmt", parent_ptr, {}),
-      compound_stmt_(compound_stmt)
+    : Node("ExpressionStmt", nullptr, {}),
+      stmt_range_(stmt_range),
+      expr_range_(expr_range)
     {}
 
     void print_node() override {
-        std::cout << node_name << '\n';
+        std::cout << node_name << "\n";
     }
 
 private:
-    std::tuple<TokenIter, TokenIter> compound_stmt_;
+    std::tuple<TokenIter, TokenIter> stmt_range_;
+    std::tuple<TokenIter, TokenIter> expr_range_;
 };
 
-//----------------Statements----------------
-class Statement : public Node {
+class NullStmt : public Node {
 public:
-    Statement(std::string node_name, Node* parent = nullptr)
-    : Node(std::move(node_name), parent, {})
+    NullStmt(std::tuple<TokenIter, TokenIter> stmt_range)
+    : Node("NullStmt", nullptr, {}),
+      stmt_range_(stmt_range)
     {}
 
+    void print_node() override {
+        std::cout << node_name << "\n";
+    }
+
+private:
+    std::tuple<TokenIter, TokenIter> stmt_range_;
+};
+
+class IfStmt : public Node {
+public:
+    IfStmt(
+        std::tuple<TokenIter, TokenIter> stmt_range,
+        std::tuple<TokenIter, TokenIter> condition_range,
+        bool has_else
+    )
+    : Node("IfStmt", nullptr, {}),
+      stmt_range_(stmt_range),
+      condition_range_(condition_range),
+      has_else_(has_else)
+    {}
+
+    void print_node() override {
+        std::cout << node_name << "\n";
+    }
+
+private:
+    std::tuple<TokenIter, TokenIter> stmt_range_;
+    std::tuple<TokenIter, TokenIter> condition_range_;
+    bool has_else_;
+};
+
+class ForStmt : public Node {
+public:
+    ForStmt(
+        std::tuple<TokenIter, TokenIter> stmt_range,
+        std::tuple<TokenIter, TokenIter> init_range,
+        std::tuple<TokenIter, TokenIter> cond_range,
+        std::tuple<TokenIter, TokenIter> step_range,
+        bool has_init,
+        bool has_cond,
+        bool has_step
+    )
+    : Node("ForStmt", nullptr, {}),
+      stmt_range_(stmt_range),
+      init_range_(init_range),
+      cond_range_(cond_range),
+      step_range_(step_range),
+      has_init_(has_init),
+      has_cond_(has_cond),
+      has_step_(has_step)
+    {}
+
+    void print_node() override {
+        std::cout << node_name << "\n";
+    }
+
+private:
+    std::tuple<TokenIter, TokenIter> stmt_range_;
+    std::tuple<TokenIter, TokenIter> init_range_;
+    std::tuple<TokenIter, TokenIter> cond_range_;
+    std::tuple<TokenIter, TokenIter> step_range_;
+
+    bool has_init_;
+    bool has_cond_;
+    bool has_step_;
+};
+
+class ReturnStmt : public Node {
+public:
+    ReturnStmt(
+        std::tuple<TokenIter, TokenIter> stmt_range,
+        std::tuple<TokenIter, TokenIter> expr_range,
+        bool has_expr
+    )
+    : Node("ReturnStmt", nullptr, {}),
+      stmt_range_(stmt_range),
+      expr_range_(expr_range),
+      has_expr_(has_expr)
+    {}
+
+    void print_node() override {
+        std::cout << node_name << "\n";
+    }
+
+private:
+    std::tuple<TokenIter, TokenIter> stmt_range_;
+    std::tuple<TokenIter, TokenIter> expr_range_;
+    bool has_expr_;
+};
+
+class BreakStmt : public Node {
+public:
+    BreakStmt(std::tuple<TokenIter, TokenIter> stmt_range)
+    : Node("BreakStmt", nullptr, {}),
+      stmt_range_(stmt_range)
+    {}
+
+    void print_node() override {
+        std::cout << node_name << "\n";
+    }
+
+private:
+    std::tuple<TokenIter, TokenIter> stmt_range_;
+};
+
+class ContinueStmt : public Node {
+public:
+    ContinueStmt(std::tuple<TokenIter, TokenIter> stmt_range)
+    : Node("ContinueStmt", nullptr, {}),
+      stmt_range_(stmt_range)
+    {}
+
+    void print_node() override {
+        std::cout << node_name << "\n";
+    }
+
+private:
+    std::tuple<TokenIter, TokenIter> stmt_range_;
 };
 
 //----------------Expressions----------------
-class Expression : public Node {
-public:
-    Expression(std::string node_name, Node* parent = nullptr)
-    : Node(std::move(node_name), parent, {})
-    {}
-
-};
-
 class IdentifierExpr : public Node {
 public:
     IdentifierExpr(TokenIter ident_iter)
@@ -255,6 +369,7 @@ public:
     using TokenList = std::vector<Lexer::Token>;
     using TokenIter = TokenList::iterator;
     using ChildNode = std::vector<std::unique_ptr<Node>>;
+    using ExprRange = std::tuple<TokenIter, TokenIter>;
 
     Parser(Lexer lexer)
     : token_list_(lexer.get_token_list())
@@ -495,9 +610,472 @@ private:
 
         return parameter_list;
     }
+    // STATEMENTS
+    void expect_token(
+        TokenIter token_iter,
+        Lexer::TokenType expected_type,
+        const std::string& error_msg
+    ) {
+        if (token_iter == token_list_.end() || token_iter->type != expected_type) {
+            print_error(token_iter, error_msg);
+            throw std::runtime_error(error_msg);
+        }
+    }
+
+    TokenIter find_top_level_semicolon(TokenIter begin, TokenIter end_iter) {
+        int paren_depth = 0;
+        int bracket_depth = 0;
+
+        for (auto iter = begin; iter != end_iter; ++iter) {
+            if (iter->type == Lexer::TokenType::TOKEN_L_PARENTH) {
+                ++paren_depth;
+            }
+            else if (iter->type == Lexer::TokenType::TOKEN_R_PARENTH) {
+                --paren_depth;
+            }
+            else if (iter->type == Lexer::TokenType::TOKEN_L_BRACKET) {
+                ++bracket_depth;
+            }
+            else if (iter->type == Lexer::TokenType::TOKEN_R_BRACKET) {
+                --bracket_depth;
+            }
+
+            if (paren_depth == 0 &&
+                bracket_depth == 0 &&
+                iter->type == Lexer::TokenType::TOKEN_SEMICOLON)
+            {
+                return iter;
+            }
+        }
+
+        return end_iter;
+    }
+
+    std::unique_ptr<Node> parse_statement(TokenIter& token_iter, TokenIter end_iter) {
+        if (token_iter == end_iter) {
+            print_error(token_iter, "Expected statement.");
+            throw std::runtime_error("Expected statement");
+        }
+
+        switch (token_iter->type) {
+            case Lexer::TokenType::TOKEN_L_BRACE:
+                return parse_compound_statement(token_iter);
+
+            case Lexer::TokenType::TOKEN_SEMICOLON:
+                return parse_null_statement(token_iter);
+
+            case Lexer::TokenType::TOKEN_IF:
+                return parse_if_statement(token_iter, end_iter);
+
+            case Lexer::TokenType::TOKEN_FOR:
+                return parse_for_statement(token_iter, end_iter);
+
+            case Lexer::TokenType::TOKEN_RETURN:
+                return parse_return_statement(token_iter, end_iter);
+
+            case Lexer::TokenType::TOKEN_BREAK:
+                return parse_break_statement(token_iter, end_iter);
+
+            case Lexer::TokenType::TOKEN_CONTINUE:
+                return parse_continue_statement(token_iter, end_iter);
+
+            case Lexer::TokenType::TOKEN_ELSE:
+                print_error(token_iter, "Unexpected 'else' without matching 'if'.");
+                throw std::runtime_error("Unexpected else");
+
+            default:
+                return parse_expression_statement(token_iter, end_iter);
+        }
+    }
+
+    std::unique_ptr<CompoundStmt> parse_compound_statement(TokenIter& token_iter) {
+        auto compound_range = find_brackets_pair(
+            token_iter,
+            Lexer::TokenType::TOKEN_L_BRACE,
+            Lexer::TokenType::TOKEN_R_BRACE,
+            "Expected '{' to start compound statement.",
+            "Expected matching '}' for compound statement."
+        );
+
+        auto compound_node = std::make_unique<CompoundStmt>(
+            nullptr,
+            compound_range
+        );
+
+        auto stmt_iter = std::get<0>(compound_range) + 1;
+        auto compound_end = std::get<1>(compound_range);
+
+        while (stmt_iter < compound_end) {
+            compound_node->add_child(
+                parse_statement(stmt_iter, compound_end)
+            );
+        }
+
+        token_iter = compound_end + 1;
+
+        return compound_node;
+    }
+
+    std::unique_ptr<Node> parse_expression_statement(
+        TokenIter& token_iter,
+        TokenIter end_iter
+    ) {
+        auto stmt_begin = token_iter;
+
+        auto semicolon_iter = find_top_level_semicolon(token_iter, end_iter);
+
+        if (semicolon_iter == end_iter) {
+            print_error(token_iter, "Expected ';' after expression.");
+            throw std::runtime_error("Expected semicolon after expression");
+        }
+
+        auto stmt_range = std::make_tuple(stmt_begin, semicolon_iter);
+        auto expr_range = std::make_tuple(stmt_begin, semicolon_iter - 1);
+
+        auto expr_stmt = std::make_unique<ExpressionStmt>(
+            stmt_range,
+            expr_range
+        );
+
+        expr_stmt->add_child(
+            parse_expr(expr_range)
+        );
+
+        token_iter = semicolon_iter + 1;
+
+        return expr_stmt;
+    }
+
+    std::unique_ptr<Node> parse_null_statement(TokenIter& token_iter) {
+        auto stmt_begin = token_iter;
+
+        expect_token(
+            token_iter,
+            Lexer::TokenType::TOKEN_SEMICOLON,
+            "Expected ';'."
+        );
+
+        auto stmt_range = std::make_tuple(stmt_begin, token_iter);
+
+        ++token_iter;
+
+        return std::make_unique<NullStmt>(stmt_range);
+    }
+
+    std::unique_ptr<Node> parse_if_statement(
+        TokenIter& token_iter,
+        TokenIter end_iter
+    ) {
+        auto stmt_begin = token_iter;
+
+        expect_token(
+            token_iter,
+            Lexer::TokenType::TOKEN_IF,
+            "Expected 'if'."
+        );
+
+        ++token_iter;
+
+        auto condition_full_range = find_brackets_pair(
+            token_iter,
+            Lexer::TokenType::TOKEN_L_PARENTH,
+            Lexer::TokenType::TOKEN_R_PARENTH,
+            "Expected '(' after 'if'.",
+            "Expected ')' after if condition."
+        );
+
+        auto cond_begin = std::get<0>(condition_full_range) + 1;
+        auto cond_end = std::get<1>(condition_full_range) - 1;
+
+        if (cond_begin > cond_end) {
+            print_error(std::get<0>(condition_full_range), "If condition cannot be empty.");
+            throw std::runtime_error("Empty if condition");
+        }
+
+        auto condition_range = std::make_tuple(cond_begin, cond_end);
+        auto condition_node = parse_expr(condition_range);
+
+        token_iter = std::get<1>(condition_full_range) + 1;
+
+        if (token_iter == end_iter) {
+            print_error(token_iter, "Expected statement after if condition.");
+            throw std::runtime_error("Expected if body");
+        }
+
+        auto then_stmt = parse_statement(token_iter, end_iter);
+
+        bool has_else = false;
+        std::unique_ptr<Node> else_stmt = nullptr;
+
+        if (token_iter != end_iter &&
+            token_iter->type == Lexer::TokenType::TOKEN_ELSE)
+        {
+            has_else = true;
+            ++token_iter;
+
+            if (token_iter == end_iter) {
+                print_error(token_iter, "Expected statement after 'else'.");
+                throw std::runtime_error("Expected else body");
+            }
+
+            else_stmt = parse_statement(token_iter, end_iter);
+        }
+
+        auto stmt_end = token_iter - 1;
+
+        auto if_node = std::make_unique<IfStmt>(
+            std::make_tuple(stmt_begin, stmt_end),
+            condition_range,
+            has_else
+        );
+
+        if_node->add_child(std::move(condition_node));
+        if_node->add_child(std::move(then_stmt));
+
+        if (has_else) {
+            if_node->add_child(std::move(else_stmt));
+        }
+
+        return if_node;
+    }
+
+    std::unique_ptr<Node> parse_for_statement(
+        TokenIter& token_iter,
+        TokenIter end_iter
+    ) {
+        auto stmt_begin = token_iter;
+
+        expect_token(
+            token_iter,
+            Lexer::TokenType::TOKEN_FOR,
+            "Expected 'for'."
+        );
+
+        ++token_iter;
+
+        auto header_full_range = find_brackets_pair(
+            token_iter,
+            Lexer::TokenType::TOKEN_L_PARENTH,
+            Lexer::TokenType::TOKEN_R_PARENTH,
+            "Expected '(' after 'for'.",
+            "Expected ')' after for header."
+        );
+
+        auto header_begin = std::get<0>(header_full_range) + 1;
+        auto header_end = std::get<1>(header_full_range) - 1;
+
+        TokenIter first_semicolon = token_list_.end();
+        TokenIter second_semicolon = token_list_.end();
+
+        int paren_depth = 0;
+        int bracket_depth = 0;
+
+        for (auto iter = header_begin; iter <= header_end; ++iter) {
+            if (iter->type == Lexer::TokenType::TOKEN_L_PARENTH) {
+                ++paren_depth;
+            }
+            else if (iter->type == Lexer::TokenType::TOKEN_R_PARENTH) {
+                --paren_depth;
+            }
+            else if (iter->type == Lexer::TokenType::TOKEN_L_BRACKET) {
+                ++bracket_depth;
+            }
+            else if (iter->type == Lexer::TokenType::TOKEN_R_BRACKET) {
+                --bracket_depth;
+            }
+
+            if (paren_depth == 0 &&
+                bracket_depth == 0 &&
+                iter->type == Lexer::TokenType::TOKEN_SEMICOLON)
+            {
+                if (first_semicolon == token_list_.end()) {
+                    first_semicolon = iter;
+                }
+                else {
+                    second_semicolon = iter;
+                    break;
+                }
+            }
+        }
+
+        if (first_semicolon == token_list_.end() ||
+            second_semicolon == token_list_.end())
+        {
+            print_error(std::get<0>(header_full_range), "For header must contain two semicolons.");
+            throw std::runtime_error("Invalid for header");
+        }
+
+        bool has_init = header_begin < first_semicolon;
+        bool has_cond = first_semicolon + 1 < second_semicolon;
+        bool has_step = second_semicolon + 1 <= header_end;
+
+        ExprRange init_range = has_init
+            ? std::make_tuple(header_begin, first_semicolon - 1)
+            : std::make_tuple(first_semicolon, first_semicolon);
+
+        ExprRange cond_range = has_cond
+            ? std::make_tuple(first_semicolon + 1, second_semicolon - 1)
+            : std::make_tuple(second_semicolon, second_semicolon);
+
+        ExprRange step_range = has_step
+            ? std::make_tuple(second_semicolon + 1, header_end)
+            : std::make_tuple(std::get<1>(header_full_range), std::get<1>(header_full_range));
+
+        std::unique_ptr<Node> init_node = nullptr;
+        std::unique_ptr<Node> cond_node = nullptr;
+        std::unique_ptr<Node> step_node = nullptr;
+
+        if (has_init) {
+            init_node = parse_expr(init_range);
+        }
+
+        if (has_cond) {
+            cond_node = parse_expr(cond_range);
+        }
+
+        if (has_step) {
+            step_node = parse_expr(step_range);
+        }
+
+        token_iter = std::get<1>(header_full_range) + 1;
+
+        if (token_iter == end_iter) {
+            print_error(token_iter, "Expected statement after for header.");
+            throw std::runtime_error("Expected for body");
+        }
+
+        auto body_stmt = parse_statement(token_iter, end_iter);
+
+        auto stmt_end = token_iter - 1;
+
+        auto for_node = std::make_unique<ForStmt>(
+            std::make_tuple(stmt_begin, stmt_end),
+            init_range,
+            cond_range,
+            step_range,
+            has_init,
+            has_cond,
+            has_step
+        );
+
+        if (has_init) {
+            for_node->add_child(std::move(init_node));
+        }
+
+        if (has_cond) {
+            for_node->add_child(std::move(cond_node));
+        }
+
+        if (has_step) {
+            for_node->add_child(std::move(step_node));
+        }
+
+        for_node->add_child(std::move(body_stmt));
+
+        return for_node;
+    }
+
+    std::unique_ptr<Node> parse_return_statement(
+        TokenIter& token_iter,
+        TokenIter end_iter
+    ) {
+        auto stmt_begin = token_iter;
+
+        expect_token(
+            token_iter,
+            Lexer::TokenType::TOKEN_RETURN,
+            "Expected 'return'."
+        );
+
+        ++token_iter;
+
+        auto semicolon_iter = find_top_level_semicolon(token_iter, end_iter);
+
+        if (semicolon_iter == end_iter) {
+            print_error(stmt_begin, "Expected ';' after return statement.");
+            throw std::runtime_error("Expected semicolon after return");
+        }
+
+        bool has_expr = token_iter < semicolon_iter;
+
+        ExprRange expr_range = has_expr
+            ? std::make_tuple(token_iter, semicolon_iter - 1)
+            : std::make_tuple(semicolon_iter, semicolon_iter);
+
+        auto return_node = std::make_unique<ReturnStmt>(
+            std::make_tuple(stmt_begin, semicolon_iter),
+            expr_range,
+            has_expr
+        );
+
+        if (has_expr) {
+            return_node->add_child(
+                parse_expr(expr_range)
+            );
+        }
+
+        token_iter = semicolon_iter + 1;
+
+        return return_node;
+    }
+
+    std::unique_ptr<Node> parse_break_statement(
+        TokenIter& token_iter,
+        TokenIter end_iter
+    ) {
+        auto stmt_begin = token_iter;
+
+        expect_token(
+            token_iter,
+            Lexer::TokenType::TOKEN_BREAK,
+            "Expected 'break'."
+        );
+
+        ++token_iter;
+
+        expect_token(
+            token_iter,
+            Lexer::TokenType::TOKEN_SEMICOLON,
+            "Expected ';' after break."
+        );
+
+        auto stmt_end = token_iter;
+        auto stmt_range = std::make_tuple(stmt_begin, stmt_end);
+
+        ++token_iter;
+
+        return std::make_unique<BreakStmt>(stmt_range);
+    }
+
+    std::unique_ptr<Node> parse_continue_statement(
+        TokenIter& token_iter,
+        TokenIter end_iter
+    ) {
+        auto stmt_begin = token_iter;
+
+        expect_token(
+            token_iter,
+            Lexer::TokenType::TOKEN_CONTINUE,
+            "Expected 'continue'."
+        );
+
+        ++token_iter;
+
+        expect_token(
+            token_iter,
+            Lexer::TokenType::TOKEN_SEMICOLON,
+            "Expected ';' after continue."
+        );
+
+        auto stmt_end = token_iter;
+        auto stmt_range = std::make_tuple(stmt_begin, stmt_end);
+
+        ++token_iter;
+
+        return std::make_unique<ContinueStmt>(stmt_range);
+    }
 
     // EXPRASSIONS
-    using ExprRange = std::tuple<TokenIter, TokenIter>;
 
     bool is_postfix_operator(TokenIter iter) {
         return iter->type == Lexer::TokenType::TOKEN_INC ||
@@ -540,7 +1118,7 @@ private:
         throw std::runtime_error("Invalid primary expression");
     }
 
-    std::unique_ptr<ArgumentListExpr> parse_argument_list(ExprRange args) {
+    std::unique_ptr<Node> parse_argument_list(ExprRange args) {
         auto arg_list = std::make_unique<ArgumentListExpr>();
 
         auto begin = std::get<0>(args);
